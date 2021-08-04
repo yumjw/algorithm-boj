e649434d29a168a3aa2347f08b6ae919dd86aa63
JungwoonYeum <ski02049@naver.com>
2021-08-04 13:05:15 +0900

    210804

---
+++ Greedy/(X) 행렬.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+A = []
+B = []
+count = 0
+
+for i in range(N):
+    row_A = list(map(int, list(input())))
+    A.append(row_A)
+
+for i in range(N):
+    row_B = list(map(int, list(input())))
+    B.append(row_B)
+
+
